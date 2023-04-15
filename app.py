b796be06e1f9bd5652e3249711d994d0bd621948
Jovana <jovanalazarevic394@gmail.com>
2023-04-15 14:07:56 +0200

    Dodate biblioteke za rad sa bazom

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import os
+import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
-
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
 @app.get("/")
 def home():
