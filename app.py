a103ae37e0ba203ad5a0673438e2f01c1fc0ef6c
Zarko <zarkocolic1998@gmail.com>
2023-04-15 14:27:00 +0200

    Napravljene tabele

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ CREATE_SOBE_TABLE = (
     "CREATE TABLE IF NOT EXISTS sobe (id SERIAL PRIMARY KEY, ime TEXT);"
 )
 
+CREATE_VLAZNOSTVAZDUHA_TABLE = """CREATE TABLE IF NOT EXISTS vlaznostvazduha (soba_id INTEGER, vlaznostvazduhasobe REAL,
+                         datum TIMESTAMP, FOREIGN KEY(soba_id) REFERENCES sobe(id) ON DELETE CASCADE);"""
+
+INSERT_SOBA_RETURN_ID = "INSERT INTO sobe (ime) VALUES (%s) RETURNING id;"
+
+INSERT_VV = (
+    "INSERT INTO vlaznostvazduha (soba_id, vlaznostvazduhasobe, datum) VALUES (%s, %s, %s);"
+)
+
 load_dotenv()
 
 app = Flask(__name__)
