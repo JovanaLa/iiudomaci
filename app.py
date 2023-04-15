d954d04864123163cac93f5aba97b9c2b69a956e
Jovana <jovanalazarevic394@gmail.com>
2023-04-15 14:12:14 +0200

    Kreirana tabela sobe

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
+
+CREATE_SOBE_TABLE = (
+    "CREATE TABLE IF NOT EXISTS sobe (id SERIAL PRIMARY KEY, ime TEXT);"
+)
+
 load_dotenv()
 
 app = Flask(__name__)
